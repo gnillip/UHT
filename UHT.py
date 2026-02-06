67f750a24db17fa5fd90f1d6da48e395260ba61a
gnillip <bennettpilling@gmail.com>
2026-02-06 17:25:49 +0100

    general build, with just 3 funcs

---
+++ UHT.py
@@ -0,0 +1,190 @@
+import func, colorama, time
+
+colorama.init()
+
+func.generall.clear()
+print(colorama.Fore.GREEN)
+print(func.generall.display_header(" UHT "))
+
+while True:
+    try:
+        print(colorama.Fore.GREEN)
+        print("\n", func.os.getcwd())
+        CMD:str = func.generall.ask("-> ", colorama.Fore.GREEN)
+
+        if CMD == "help":
+            print(f"{colorama.Fore.BLUE}commands:")
+            print(f"""
+exit                (to exit the program)
+port-scan           (for scanning ports of an IP address)
+socket              (for socket communication)
+    - socket::enc   (for encrypted communication between 2 UHT's)
+    - socket::uni   (for universal communication, where you can send and recieve an Answer)
+encrypt             (to encrypt a file)
+decrypt             (to decrypt a file, which was encrypted before)
+net                 (for locking / unlocking    usage: net [username] --lock or net [username] --unlock)
+passwd              (to change password     usage: passwd [username] [new password])
+{colorama.Fore.GREEN}""")
+        
+        elif CMD == "exit":
+            func.generall.clear()
+            print("You chose exit...")
+            break
+
+        elif CMD == "port-scan":
+            IP = func.generall.ask("IP: ", colorama.Fore.GREEN)
+            PORTS = {
+                20: "FTP-Datenübertragung",
+                21: "FTP-Steuerverbindung",
+                22: "SSH – sichere Remote-Verbindung",
+                23: "Telnet – unverschlüsselte Remote-Shell",
+                25: "SMTP – E-Mail-Versand",
+                53: "DNS – Namensauflösung",
+                67: "DHCP-Server",
+                68: "DHCP-Client",
+                80: "HTTP – Webserver (unverschlüsselt)",
+                110: "POP3 – E-Mail-Abruf",
+                143: "IMAP – E-Mail-Abruf",
+                443: "HTTPS – Webserver (verschlüsselt)",
+                3306: "MySQL-Datenbank",
+                5432: "PostgreSQL-Datenbank",
+                6379: "Redis – In-Memory-Datenbank",
+                8080: "Alternativer HTTP-Port / Web-Proxy"
+            }
+
+            PORT_GESCHW:str = func.generall.ask("Fast / Normal / Slow: ", colorama.Fore.GREEN)
+            PORT_GESCHW = PORT_GESCHW.lower()
+
+            print("[*] PORT selection")
+            if PORT_GESCHW == "fast":
+                pass
+            elif PORT_GESCHW == "normal":
+                for i in range(1, 1025):
+                    if i not in PORTS:
+                        PORTS[i] = ""
+            elif PORT_GESCHW == "slow":
+                for i in range(1, 65537):
+                    if i not in PORTS:
+                        PORTS[i] = ""
+            
+            print("[*] now scanning")
+            time.sleep(0.5)
+
+            aktive = []
+            for port in PORTS:
+                sock:func.socket.socket = func.net.create_TCP()
+                sock.settimeout(0.2)
+                res = sock.connect_ex((IP, port))
+                sock.close()
+
+                if res == 0:
+                    aktive.append(port)
+                    print("[*] open port: ", port)
+            
+            print("[*] now conclusion: port list, with description to some known ports")
+            for port in aktive:
+                print(f"     {port} - {PORTS[port]}")
+
+        elif CMD.startswith("socket"):
+            if CMD == "socket::enc":
+                Client_Server:str = func.generall.ask("Client or Server? ", colorama.Fore.GREEN)
+
+                if Client_Server.lower() == "client":
+                    client:func.socket.socket = func.net.create_TCP()
+                    IP:str = func.generall.ask("IP: ", colorama.Fore.GREEN)
+                    PORT:str = func.generall.ask("PORT: ", colorama.Fore.GREEN)
+                    client.connect((IP, int(PORT)))
+
+                    p_len = int.from_bytes(func.net.recv_exact(client, 8), "big")
+                    p = func.net.recv_exact(client, p_len)
+                    g_len = int.from_bytes(func.net.recv_exact(client, 8), "big")
+                    g = func.net.recv_exact(client, g_len)
+                    DH_KEY = func.net.DH(int(p), int(g))
+
+                    their_pub_key_len = int.from_bytes(func.net.recv_exact(client, 8), "big")
+                    their_pub_key = func.net.recv_exact(client, their_pub_key_len)
+
+                    pub_bytes = DH_KEY.public_key.to_bytes((DH_KEY.public_key.bit_length() +7) // 8, byteorder="big")
+                    client.sendall(len(pub_bytes).to_bytes(8, "big") + pub_bytes)
+
+                    DH_KEY.generate_other(int.from_bytes(their_pub_key, "big"))
+
+                    while True:
+                        txt:str = func.generall.ask("\nYour Message (or exit): ", colorama.Fore.BLUE)
+                        txt = DH_KEY.encrypt(txt.encode())
+                        client.sendall(len(txt).to_bytes(8, "big") + txt)
+
+                        if DH_KEY.decrypt(txt) == b"exit":
+                            client.close()
+                            break
+
+                        ans_len = int.from_bytes(func.net.recv_exact(client, 8), "big")
+                        ans = func.net.recv_exact(client, ans_len)
+                        ans = DH_KEY.decrypt(ans).decode()
+                        print("\n", colorama.Fore.RED, ans)
+
+                        if ans == "exit":
+                            client.close()
+                            break
+                    print(colorama.Fore.GREEN)
+
+                elif Client_Server.lower() == "server":
+                    server:func.socket.socket = func.net.create_TCP()
+                    PORT:str = func.generall.ask("PORT: ", colorama.Fore.GREEN)
+                    server.bind(("0.0.0.0", int(PORT)))
+                    server.listen(1)
+
+                    conn, addr = server.accept()
+                    print(f"{colorama.Fore.BLUE}[*] connection from {addr}")
+                    p = str(0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1).encode()
+                    g = str(2).encode()
+                    conn.sendall(len(p).to_bytes(8, "big") + p)
+                    conn.sendall(len(g).to_bytes(8, "big") + g)
+
+                    DH_KEY = func.net.DH(int(p), int(g))
+
+                    pub_bytes = DH_KEY.public_key.to_bytes((DH_KEY.public_key.bit_length() +7) // 8, byteorder="big")
+
+                    conn.sendall(len(pub_bytes).to_bytes(8, "big") + pub_bytes)
+                    their_pub_key_len = int.from_bytes(func.net.recv_exact(conn, 8), "big")
+                    their_pub_key = func.net.recv_exact(conn, their_pub_key_len)
+
+                    DH_KEY.generate_other(int.from_bytes(their_pub_key, "big"))
+
+                    while True:
+                        ans_len = int.from_bytes(func.net.recv_exact(conn, 8), "big")
+                        ans = func.net.recv_exact(conn, ans_len)
+                        ans = DH_KEY.decrypt(ans).decode()
+                        print("\n", colorama.Fore.RED, ans)
+
+                        if ans == "exit":
+                            conn.close()
+                            break
+
+                        txt:str = func.generall.ask("\nYour Text (or exit): ", colorama.Fore.BLUE)
+                        txt = DH_KEY.encrypt(txt.encode())
+                        conn.sendall(len(txt).to_bytes(8, "big") + txt)
+
+                        if DH_KEY.decrypt(txt) == b"exit":
+                            conn.close()
+                            break
+                    print(colorama.Fore.GREEN)
+
+                else:
+                    print("This wasn't an Option!")
+                    continue
+
+            elif CMD == "socket::uni":
+                pass
+
+            else:
+                print("This wasn't an Option!")
+                continue
+
+        else:
+            print("This wasn't an Option!")
+
+    except KeyboardInterrupt:
+        func.generall.clear()
+        print("KeyboardInterrupt")
+        break
---
+++ func.py
@@ -0,0 +1,155 @@
+import os, socket, secrets, hashlib, json, colorama
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
+class generall:
+    def clear() -> None:
+        """
+        Clears the Terminal-Screen
+        """
+        if os.name == "nt":
+            os.system("cls")
+        else:
+            os.system("clear")
+    
+    def display_header(text:str) -> str:
+        """       
+        :param text: the Text, which should be displayed
+        :type text: str
+        :return: a 3 line string, which you can print to the Terminal
+        :rtype: str
+        """
+        liste = [
+            "="*(len(text)+2),
+            f"|{text}|",
+            "="*(len(text)+2)
+        ]
+        return "\n".join(liste)
+    
+    def ask(question:str, out_color:str) -> str:
+        """
+        :param question: input(question)
+        :type question: str
+        :param out_color: colorama.Fore.{var} | the ending Color
+        :type out_color: str
+        :return: the answer of input()
+        :rtype: str
+        """
+        var = input(f"{colorama.Fore.RESET}{question}{colorama.Fore.RED}")
+        print(out_color)
+        return var
+    
+    def read(filename:str, isJson:bool) -> str|dict:
+        """
+        :param filename: The name with path of the File
+        :type filename: str
+        :param isJson: if: dict -> True else: -> False
+        :type isJson: bool
+        :return: if: isJson -> dict else: -> string
+        :rtype: str | dict
+        """
+        if not os.path.exists(filename):
+            raise FileNotFoundError("This File/Path doesn't exist!")
+
+        with open(filename, "r") as data:
+
+            if isJson:
+                return json.load(data)
+            
+            else:
+                return data.read()
+    
+    def write(filename:str, content:dict|list|str, isJson:bool) -> None:
+        """
+        :param filename: The name with Path of the File
+        :type filename: str
+        :param content: What should be in the File (if isJson: -> dict else: str or list)
+        :type content: dict | list | str
+        :param isJson: if the output format should be a JSON type
+        :type isJson: bool
+        """
+        if not os.path.exists(filename):
+            raise FileNotFoundError("This File/Path doesn't exist!")
+        
+        with open(filename, "w") as data:
+
+            if isJson:
+                json.dump(content, data, indent=True)
+            
+            else:
+                if type(content) == str:
+                    data.write(content)
+                else:
+                    for line in content:
+                        data.write(line)
+
+class net:
+    """
+    Networking Functions etc.
+    """
+
+    def create_TCP() -> socket.socket:
+        return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    def recv_exact(sock:socket.socket, n:int) -> bytes:
+        """        
+        :param sock: the socket obj
+        :type sock: socket.socket
+        :param n: the byte count
+        :type n: int
+        :return: the content of the packages
+        :rtype: bytes
+        """
+        data = b""
+        while len(data) < n:
+            chunk = sock.recv(n-len(data))
+            if not chunk:
+                raise ConnectionError("Socket closed")
+            data += chunk
+        return data
+
+    class DH:
+        """
+        A class to work with, if you need to encrypt Traffic
+        """
+        def __init__(self, p:int, g:int):
+            """
+            :param p: A (big) prime integer
+            :type p: int
+            :param g: Generator, where g<p (mostly 2 or 5)
+            :type g: int
+            """
+            self.p = p
+            self.g = g
+
+            self.private_key = secrets.randbelow(self.p)
+            self.public_key = pow(g, self.private_key, self.p)
+
+            self.shared_secret = None
+            self.AES_KEY = None
+        
+        def generate_other(self, their_public_key:int) -> None:
+            self.shared_secret = pow(their_public_key, self.private_key, self.p)
+            self.shared_secret__bytes = self.shared_secret.to_bytes((self.shared_secret.bit_length()+7)//8, byteorder="big")
+            self.AES_KEY = hashlib.sha256(self.shared_secret__bytes).digest()
+        
+        def encrypt(self, text:bytes) -> bytes:
+            if not self.AES_KEY:
+                raise SystemError("AES_KEY doesn't exist!")
+            
+            aesgcm = AESGCM(self.AES_KEY)
+            nonce = os.urandom(12)
+            cipher = aesgcm.encrypt(nonce, text, None)
+            return nonce + cipher
+        
+        def decrypt(self, data:bytes) -> bytes:
+            if not self.AES_KEY:
+                raise SystemError("AES_KEY doesn't exist!")
+            
+            aesgcm = AESGCM(self.AES_KEY)
+            nonce = data[:12]
+            cipher = data[12:]
+            return aesgcm.decrypt(nonce, cipher, None)
+    
+if __name__ == "__main__":
+    generall.clear()
+    print(generall.display_header("test in func.py"))
